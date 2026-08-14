6f47c17128b1b8eaf1ebf358edfc53743e8c3b2b
Rodwan26 <rdwanbnmwsy064@gmail.com>
2026-08-14 23:57:29 +0100

    Update project

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "../components/landing/header";
+import Header from "../components/landing/Header";
 import Hero from "../components/landing/Hero/Hero";
 import Features from "../components/landing/Features/Features";
 import OrderForm from "../components/landing/Order/OrderForm";
