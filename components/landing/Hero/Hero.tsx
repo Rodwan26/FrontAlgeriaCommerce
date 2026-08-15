f2fa98196b329037e732d4ff9054073b2aae41bd
Rodwan26 <rdwanbnmwsy064@gmail.com>
2026-08-15 12:14:15 +0100

    Update project

--- components/landing/Hero/Hero.tsx
+++ components/landing/Hero/Hero.tsx
@@ -22,29 +22,98 @@ export default function Hero() {
       <div className="mx-auto flex max-w-6xl flex-col px-4 pb-8 pt-8 md:flex-row md:items-center md:gap-12 md:py-16">
 
         {/* Product Image */}
-        <div className="relative order-1 w-full md:order-2 md:w-1/2">
+       {/* Product Visual */}
+<div className="relative order-1 w-full md:order-2 md:w-1/2">
+  <div className="relative mx-auto flex aspect-square w-full max-w-[390px] items-center justify-center">
 
-          <div className="absolute inset-10 rounded-full bg-red-600/20 blur-[70px]" />
+    {/* Main ambient glow */}
+    <div
+      className="
+        absolute
+        left-1/2
+        top-1/2
+        h-[65%]
+        w-[65%]
+        -translate-x-1/2
+        -translate-y-1/2
+        rounded-full
+        bg-red-600/30
+        blur-[90px]
+      "
+    />
 
-          <div className="relative">
-            <Image
-              src={product.heroImage}
-              alt={product.name}
-              width={700}
-              height={600}
-              priority
-              className="relative z-10 mx-auto w-full max-w-[370px] object-contain drop-shadow-[0_20px_50px_rgba(239,68,68,0.3)] md:max-w-[600px]"
-            />
-          </div>
+    {/* Secondary glow */}
+    <div
+      className="
+        absolute
+        left-[42%]
+        top-[48%]
+        h-[35%]
+        w-[55%]
+        -translate-x-1/2
+        -translate-y-1/2
+        rounded-full
+        bg-orange-500/10
+        blur-[70px]
+      "
+    />
 
-          {/* Carousel indicators */}
-          <div className="relative z-20 mt-2 flex justify-center gap-2">
-            <span className="h-2.5 w-7 rounded-full bg-red-500" />
-            <span className="h-2.5 w-2.5 rounded-full bg-gray-500" />
-            <span className="h-2.5 w-2.5 rounded-full bg-gray-500" />
-          </div>
-        </div>
+    {/* Ground shadow */}
+    <div
+      className="
+        absolute
+        bottom-[17%]
+        left-1/2
+        h-8
+        w-[55%]
+        -translate-x-1/2
+        rounded-[50%]
+        bg-black/70
+        blur-2xl
+      "
+    />
 
+    {/* Shoe */}
+    <div className="relative z-10 w-[105%]">
+      <Image
+        src={product.heroImage}
+        alt={product.name}
+        width={800}
+        height={700}
+        priority
+        className="
+          h-auto
+          w-full
+          object-contain
+          drop-shadow-[0_25px_35px_rgba(0,0,0,0.55)]
+        "
+      />
+    </div>
+
+    {/* Small decorative light */}
+    <div
+      className="
+        pointer-events-none
+        absolute
+        bottom-[22%]
+        left-[20%]
+        h-2
+        w-20
+        rounded-full
+        bg-red-500/40
+        blur-xl
+      "
+    />
+
+  </div>
+
+  {/* Carousel indicators */}
+  <div className="relative z-20 mt-1 flex justify-center gap-2">
+    <span className="h-2 w-8 rounded-full bg-red-500" />
+    <span className="h-2 w-2 rounded-full bg-white/20" />
+    <span className="h-2 w-2 rounded-full bg-white/20" />
+  </div>
+</div>
         {/* Content */}
         <div className="order-2 mt-6 w-full text-right md:order-1 md:w-1/2">
 
