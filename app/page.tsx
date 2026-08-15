666a17b960888dd9a0c16a1e3cd777637c5f79d4
Rodwan26 <rdwanbnmwsy064@gmail.com>
2026-08-15 10:28:09 +0100

    Update project

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "../components/landing/Hero/Hero";
 import Features from "../components/landing/Features/Features";
 import OrderForm from "../components/landing/Order/OrderForm";
 import Footer from "../components/landing/Footer";
+
 export default function Home() {
   return (
     <main
@@ -10,13 +11,9 @@ export default function Home() {
       className="min-h-screen bg-[#080a0b]"
     >
       <Header />
-
       <Hero />
-
       <Features />
-
       <OrderForm />
-
       <Footer />
     </main>
   );
--- components/landing/Hero/Hero.tsx
+++ components/landing/Hero/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { ArrowDown } from "lucide-react";
 import { product } from "../../data/product";
